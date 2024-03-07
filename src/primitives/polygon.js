292e0d9e4ac8f06b7b63791d5b849290efbe0855
Evgeniy-Hozyainov <c0r2air@yandex.ru>
2024-03-07 12:18:34 +0300

    Разбивать все сегменты всех полигонов в точках их пересечения на несколько сегментов

--- src/primitives/polygon.js
+++ src/primitives/polygon.js
@@ -11,6 +11,14 @@ class Polygon {
     }
   }
 
+  static multiBreak(polys) {
+    for (let i = 0; i < polys.length - 1; i++) {
+      for (let j = i + 1; j < polys.length; j++) {
+        Polygon.break(polys[i], polys[j]);
+      }
+    }
+  }
+
   static break(poly1, poly2) {
     const segs1 = poly1.segments;
     const segs2 = poly2.segments;
@@ -39,7 +47,7 @@ class Polygon {
 
   drawSegments(ctx) {
     for (const seg of this.segments) {
-      seg.draw(ctx, {color: getRandomColor(), width: 5 })
+      seg.draw(ctx, { color: getRandomColor(), width: 5 });
     }
   }
 
